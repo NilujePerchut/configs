2bb2b318ad6e3904eaea8a829668ef46bf54c9d0
Cedric Debarge <cedric.debarge@acksys.fr>
2014-10-14 13:29:44 +0200

    Ajout de la configuration minum de emacs

--- install.sh
+++ install.sh
@@ -22,10 +22,22 @@ function awesome_cfg_install {
 	ln -s $HOME/configs/awesome $HOME/.config/awesome
 }
 
+function emacs_cfg_install {
+	#remove any previous configuration
+	rm -rf $HOME/.emacs.d
+	rm -f $HOME/.emacs
+	#create the new links
+	ln -s $HOME/configs/.emacs.d $HOME/.emacs.d
+	ln -s $HOME/configs/.emacs $HOME/.emacs
+
+}
+
 #install vim
 gvim_cfg_install
 #install awesome
 awesome_cfg_install
+#install emacs
+emacs_cfg_install
 
 echo Configuration files installed !
 echo Enjoy

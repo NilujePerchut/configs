80a631317fad45980bf4bfeb247402d2537987ea
niluje <debarge.cedric@gmail.com>
2016-08-18 19:44:42 +0200

    Spacemacs: Add a custom C layer

--- spacemacs_layers/cd-c/packages.el
+++ spacemacs_layers/cd-c/packages.el
@@ -1,8 +1,47 @@
 (setq cd-c-packages
-      '(cc-mode))
+      '(
+		cc-mode
+		(cc-styles :location built-in)
+		never-comment
+		))
+
+(defun cd-c/init-never-comment ()
+  (use-package never-comment
+			:defer t
+			:init
+            (never-comment-init)))
 
 (defun cd-c/post-init-cc-mode ()
-  (setq-default c-default-sytle "linux"
-    c-basic-offset 8)
-  (setq-default tab-width 8)
-  (setq-default indent-tabs-mode t))
+  (dolist (mode '(c-mode c++-mode))
+    (spacemacs/set-leader-keys-for-major-mode mode
+      "os" 'c-set-style))
+)
+
+(defun cd-c/init-cc-styles ()
+  (use-package cc-styles
+	:defer t
+	:config
+	(progn
+	  (c-add-style "cd"
+				   '((indent-tabs-mode . t)
+                     (c-tab-always-indent . t)
+                     (tab-width . 4)
+					 (c-basic-offset . 4)
+					 (c-offsets-alist
+                      (substatement-open . 0)
+                      (inline-open . 0)
+                      (statement-cont . c-lineup-assignments)
+                      (inextern-lang . 0)
+					  (innamespace . 0))))
+	  (c-add-style "kernel"
+				   '((indent-tabs-mode . t)
+           (c-tab-always-indent . t)
+					 (c-basic-offset . 8)
+                     (tab-width . 8)
+					 (c-offsets-alist
+                      (substatement-open . 0)
+                      (inline-open . 0)
+                      (statement-cont . c-lineup-assignments)
+                      (inextern-lang . 0)
+					  (innamespace . 0))))
+	  (push '(other . "cd") c-default-style))))

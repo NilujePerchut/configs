3d183e6e7e53274fadc3b887f267d041c9df98af
Cédric DEBARGE <cdebarge@nanoxplore.com>
2023-07-13 13:36:29 +0200

    nvim: Adds markdown lsp and preview

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -80,6 +80,9 @@ local servers = {
 
   bashls = {
   },
+
+  marksman = {
+  },
 }
 
 --
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -100,6 +100,13 @@ require('lazy').setup({
   'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1
   },
 
+  -- Markdown
+  { "iamcco/markdown-preview.nvim",
+    config = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+
   -- Orgmode
   "nvim-orgmode/orgmode",
   "akinsho/org-bullets.nvim",

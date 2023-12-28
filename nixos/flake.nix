4626f7ae2cd6f14d1dc40d7289472c8d1d614bf1
niluje <debarge.cedric@gmail.com>
2023-12-29 00:55:49 +0100

    nixos: intial setup (up to flake)

---
+++ nixos/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "my_flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+  outputs = { self, nixpkgs, ... }: 
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      osdegirouette = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}

f5bff3c32bf534f242f446bf29991f939ba4aeab
niluje <debarge.cedric@gmail.com>
2023-12-29 01:19:36 +0100

    nixos: add home-manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,15 +2,25 @@
   description = "my_flake";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       osdegirouette = lib.nixosSystem {
-        system = "x86_64-linux";
+        system = system;
         modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      niluje = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };

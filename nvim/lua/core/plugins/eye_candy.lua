6bfdca20289fb0b6d12b6ca5dd94e78a254a27b0
niluje <debarge.cedric@gmail.com>
2024-04-08 22:33:18 +0200

    nvim: move plugin declarations from base_plugin to dedicated files

--- nvim/lua/core/plugins/eye_candy.lua
+++ nvim/lua/core/plugins/eye_candy.lua
@@ -1,5 +1,20 @@
 -- Debugger related stuff
 return {
+	{
+    -- Theme inspired by Atom
+		"navarasu/onedark.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000,
+		option = 'dark',
+		config = function()
+			-- Load the colorscheme here
+			vim.cmd.colorscheme("onedark")
+
+			-- You can configure highlights by doing something like
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+
 	-- rainbow delimiters
 	"HiPhish/rainbow-delimiters.nvim",
 
@@ -26,5 +41,36 @@ return {
 		'lukas-reineke/indent-blankline.nvim',
 		main = 'ibl',
 		opts = {},
-	}
+	},
+
+	{ -- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"cpp",
+					"go",
+					"lua",
+					"python",
+					"rust",
+					"typescript",
+					"vimdoc",
+					"vim",
+					"bash",
+					"diff",
+					"hjson",
+					"make",
+					"markdown",
+					"verilog",
+					"org",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				ident = { enable = true },
+			})
+		end,
+	},
 }

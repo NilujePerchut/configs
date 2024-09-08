4120d776d59c5d7561b30f3babd717b57ebb7625
niluje <debarge.cedric@gmail.com>
2024-09-08 15:53:10 +0200

    nixos: add osdeframboise to hosts

--- nixos/nixosModules/bundles/hosts.nix
+++ nixos/nixosModules/bundles/hosts.nix
@@ -8,6 +8,7 @@
   networking.hosts = {
     "192.168.40.40" = ["osdegirouette"];
     "192.168.40.41" = ["osdepoulet"];
+    "192.168.40.42" = ["osdeframboise"];
     "192.168.40.44" = ["osdemammouth"];
     "192.168.40.63" = ["vmshare"];
     "192.168.40.254" = ["freebox"];

9141bf466811f43186987a07eaa119e543499069
niluje <debarge.cedric@gmail.com>
2024-10-03 23:34:29 +0200

    nixos: add osdeplacard

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,6 +17,7 @@
         osdegirouette = mkSystem ./hosts/osdegirouette/configuration.nix;
         osdepoulet = mkSystem ./hosts/osdepoulet/configuration.nix;
         osdeframboise = mkSystem ./hosts/osdeframboise/configuration.nix;
+        osdeplacard = mkSystem ./hosts/osdeplacard/configuration.nix;
       };
 
       homeConfigurations = {
@@ -24,6 +25,7 @@
         "niluje@osdegirouette" = mkHome "x86_64-linux" ./hosts/osdegirouette/home.nix;
         "niluje@osdepoulet" = mkHome "x86_64-linux" ./hosts/osdepoulet/home.nix;
         "niluje@osdeframboise" = mkHome "aarch64-linux" ./hosts/osdeframboise/home.nix;
+        "niluje@osdeplacard" = mkHome "x86_64-linux" ./hosts/osdeplacard/home.nix;
       };
 
       homeManagerModules.default = ./homeManagerModules;
---
+++ nixos/hosts/osdeplacard/configuration.nix
@@ -0,0 +1,39 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  outputs,
+  system,
+  myLib,
+  ...
+}: {
+  imports =
+    [ ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  myNixOS = {
+    bundles.default.enable = true;
+    bundles.users.enable = true;
+    bundles.hosts.enable = true;
+  };
+
+  networking = {
+    hostName = "osdeplacard";
+    networkmanager.enable = true;
+    enableIPv6 = false;
+  };
+
+  # Disable firewall since we are behind a VPN
+  networking.firewall.enable = false;
+
+  system.stateVersion = "24.05";
+}

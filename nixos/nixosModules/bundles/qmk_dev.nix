e893700ac6510e93a4d64c4c6de4b4bc3082ecd7
niluje <debarge.cedric@gmail.com>
2026-02-06 22:21:20 +0100

    nixos: Add pickit2 and saleae support
    Includes soft + udev rules.
    All these udev rules require a plugdev group => create the group

--- nixos/nixosModules/bundles/qmk_dev.nix
+++ nixos/nixosModules/bundles/qmk_dev.nix
@@ -11,11 +11,18 @@
       # Rules for keyboard web flashing and live training
       KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
       KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+      # PICkit 2
+      ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="0033", MODE="0660", GROUP="plugdev"
+      # PICkit 3
+      ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="900a", MODE="0660", GROUP="plugdev"
+      # PKOB
+      ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="8107", MODE="0660", GROUP="plugdev"
     '';
 
     packages = with pkgs; [
       via
       picoprobe-udev-rules
+      saleae-logic-2
     ];
   };
 
--- nixos/nixosModules/bundles/users.nix
+++ nixos/nixosModules/bundles/users.nix
@@ -5,11 +5,15 @@
   lib,
   ...
 }: {
+
+  # Add the plugdev group
+  users.groups.plugdev = {};
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.niluje = {
     isNormalUser = true;
     description = "niluje";
-    extraGroups = [ "networkmanager" "wheel" "plugdev" ];
+    extraGroups = [ "networkmanager" "wheel" "plugdev" "dialout"];
     packages = with pkgs; [
       zsh
     ];
